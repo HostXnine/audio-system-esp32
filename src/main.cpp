015d944f9376159947cffc01600a2cd4a8e4e3d7
hostxnine <hostx9@gmail.com>
2026-09-05 22:18:09 +0200

    - turned off stepper motor in loop

--- src/main.cpp
+++ src/main.cpp
@@ -25,5 +25,5 @@ void loop() {
     //decodeNewRemote(); //uncomment this for decoding a new remote
     irStateLoop(currentIrState);
     systemStateLoop(currentSystemState);
-    stepperLoop();
+    //stepperLoop(); // was only for testing
 }
